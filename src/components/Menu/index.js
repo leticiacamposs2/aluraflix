087a3c859e8037b7c1d338f5d71cedc2a9e4c90e
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-07-27 20:40:14 -0300

    pagina inicial

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from '../../assets/img/logo.png'
+import Button from '../Button';
 import './Menu.css';
 
 function Menu() {
@@ -8,7 +9,9 @@ function Menu() {
             <a href="/">
                 <img className="Logo" src={Logo} alt="Logo da AluraFlix" />
             </a>
-            <a className="ButtonLink" href="/">Novo vídeo</a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+            </Button>
         </nav>
     )
 }
