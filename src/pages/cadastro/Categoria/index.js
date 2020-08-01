319bb1cd91c5d3236410d757723099251f11e663
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-08-01 00:11:30 -0300

    adicionando url do heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,7 +28,7 @@ function CadastroCategoria() {
 
     useEffect(() => {
         if (window.location.href.includes('localhost')) {
-            const URL = 'http://localhost:8080/categorias';
+            const URL = 'http://leticiacamposs2-aluraflix.herokuapp.com/categorias';
             fetch(URL)
                 .then(async (respostaDoServer) => {
                     if (respostaDoServer.ok) {
